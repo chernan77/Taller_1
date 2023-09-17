8e531034c4bf92a78af6ecde41d7094d2041c675
elabordev <e.laborde@uniandes.edu.co>
2023-09-17 15:15:04 -0500

    Punto 5 sin markdown

---
+++ Punto 5.R
@@ -0,0 +1,8 @@
+## ---------------------------
+##
+## Punto 5: Predicting earnings . In the previous sections, you estimated some specifications with
+## inference in mind. In this subsection, we will evaluate the predictive power of these
+## specifications.
+##
+## ---------------------------
+
